afcae38165c882394557b69e4d0810aed1ed75ff
Fernando Hermosillo Reynoso <45222157+fhermosillo@users.noreply.github.com>
2022-09-21 14:20:25 -0500

    Update main.c

--- examples/3 - gpio_stepper/main.c
+++ examples/3 - gpio_stepper/main.c
@@ -1,10 +1,10 @@
 #include "bcm2xxx_delay.h"
 #include "bcm2xxx_hal_gpio.h"
 
-#define MOTOR_S1	GPIO_PIN_17
-#define MOTOR_S2	GPIO_PIN_27
-#define MOTOR_S3	GPIO_PIN_22
-#define MOTOR_S4	GPIO_PIN_5
+#define MOTOR_B1	GPIO_PIN_17
+#define MOTOR_B2	GPIO_PIN_27
+#define MOTOR_B3	GPIO_PIN_22
+#define MOTOR_B4	GPIO_PIN_5
 
 #define __28BYJ_48_STEPS	2038
 #define REV_PER_MICROSEC	(60*1000*1000)	/* Revolutions per microsecond */
@@ -18,17 +18,17 @@ int main()
 	// Peripheral Configurations
 	HAL_GPIO_Init();
 	
-	HAL_GPIO_Set_Mode(MOTOR_S1, GPIO_OUTPUT);
-	HAL_GPIO_Set_Mode(MOTOR_S2, GPIO_OUTPUT);
-	HAL_GPIO_Set_Mode(MOTOR_S3, GPIO_OUTPUT);
-	HAL_GPIO_Set_Mode(MOTOR_S4, GPIO_OUTPUT);
+	HAL_GPIO_Set_Mode(MOTOR_B1, GPIO_OUTPUT);
+	HAL_GPIO_Set_Mode(MOTOR_B2, GPIO_OUTPUT);
+	HAL_GPIO_Set_Mode(MOTOR_B3, GPIO_OUTPUT);
+	HAL_GPIO_Set_Mode(MOTOR_B4, GPIO_OUTPUT);
 	
 	uint32_t rpm = 12; // Max 14
 	uint32_t us = REV_PER_MICROSEC / __28BYJ_48_STEPS / rpm;
-	HAL_GPIO_Write(MOTOR_S1,GPIO_LOW);
-	HAL_GPIO_Write(MOTOR_S2,GPIO_LOW);
-	HAL_GPIO_Write(MOTOR_S3,GPIO_LOW);
-	HAL_GPIO_Write(MOTOR_S4,GPIO_LOW);
+	HAL_GPIO_Write(MOTOR_B1,GPIO_LOW);
+	HAL_GPIO_Write(MOTOR_B2,GPIO_LOW);
+	HAL_GPIO_Write(MOTOR_B3,GPIO_LOW);
+	HAL_GPIO_Write(MOTOR_B4,GPIO_LOW);
 	
 	uint32_t rpm = 0;
 	printf("Revoluciones por minuto (1 - 14): ");
@@ -45,29 +45,29 @@ int main()
 		/* Secuencia para giro en el sentido}
 		 * de las manecillas (derecha)
 		 */
-		// Estado #1
-		HAL_GPIO_Write(MOTOR_S1,GPIO_HIGH);
-		HAL_GPIO_Write(MOTOR_S2,GPIO_LOW);
-		HAL_GPIO_Write(MOTOR_S3,GPIO_HIGH);
-		HAL_GPIO_Write(MOTOR_S4,GPIO_LOW);
+		// Estado S1
+		HAL_GPIO_Write(MOTOR_B1,GPIO_HIGH);
+		HAL_GPIO_Write(MOTOR_B2,GPIO_LOW);
+		HAL_GPIO_Write(MOTOR_B3,GPIO_HIGH);
+		HAL_GPIO_Write(MOTOR_B4,GPIO_LOW);
 		delay_us(rpus);
-		// Estado #2
-		HAL_GPIO_Write(MOTOR_S1,GPIO_LOW);
-		HAL_GPIO_Write(MOTOR_S2,GPIO_HIGH);
-		HAL_GPIO_Write(MOTOR_S3,GPIO_HIGH);
-		HAL_GPIO_Write(MOTOR_S4,GPIO_LOW);
+		// Estado S2
+		HAL_GPIO_Write(MOTOR_B1,GPIO_LOW);
+		HAL_GPIO_Write(MOTOR_B2,GPIO_HIGH);
+		HAL_GPIO_Write(MOTOR_B3,GPIO_HIGH);
+		HAL_GPIO_Write(MOTOR_B4,GPIO_LOW);
 		delay_us(rpus);
-		// Estado #3
-		HAL_GPIO_Write(MOTOR_S1,GPIO_LOW);
-		HAL_GPIO_Write(MOTOR_S2,GPIO_HIGH);
-		HAL_GPIO_Write(MOTOR_S3,GPIO_LOW);
-		HAL_GPIO_Write(MOTOR_S4,GPIO_HIGH);
+		// Estado S3
+		HAL_GPIO_Write(MOTOR_B1,GPIO_LOW);
+		HAL_GPIO_Write(MOTOR_B2,GPIO_HIGH);
+		HAL_GPIO_Write(MOTOR_B3,GPIO_LOW);
+		HAL_GPIO_Write(MOTOR_B4,GPIO_HIGH);
 		delay_us(rpus);
-		// Estado #4
-		HAL_GPIO_Write(MOTOR_S1,GPIO_HIGH);
-		HAL_GPIO_Write(MOTOR_S2,GPIO_LOW);
-		HAL_GPIO_Write(MOTOR_S3,GPIO_LOW);
-		HAL_GPIO_Write(MOTOR_S4,GPIO_HIGH);
+		// Estado S4
+		HAL_GPIO_Write(MOTOR_B1,GPIO_HIGH);
+		HAL_GPIO_Write(MOTOR_B2,GPIO_LOW);
+		HAL_GPIO_Write(MOTOR_B3,GPIO_LOW);
+		HAL_GPIO_Write(MOTOR_B4,GPIO_HIGH);
 		delay_us(rpus);
 	}
 	/* USER CODE END -------------------------------------------------------------------*/
