fe35173f250a7d91152a4d34530cefb749e200fa
Fernando Hermosillo Reynoso <45222157+fhermosillo@users.noreply.github.com>
2022-09-22 13:56:15 -0500

    Update main.c

--- examples/4 - i2c_ov7670/main.c
+++ examples/4 - i2c_ov7670/main.c
@@ -3,14 +3,18 @@
 #include "bcm2xxx_hal_i2c.h"
 #include "bcm2xxx_hal_cm.h"
 
+
 // Define GPIO PINES
 #define GPIO_SDA	GPIO_PIN_2
 #define GPIO_SCL	GPIO_PIN_3
 #define GPIO_XCLK	GPIO_PIN_4
 
+
+// I2C Constants
 #define OV7670_ADDRESS		0x21
 #define OV7670_LOWER_REG	0x00
 #define OV7670_UPPER_REG	0xC9
+
 int main()
 {
 	/* OPEN PERIPHERAL'S MEMORY */
@@ -32,6 +36,7 @@ int main()
 	HAL_CM_Set_ClkSrc(CM_GPIO0, CM_SOURCE_PLLD);
 	HAL_CM_Set_Freq(CM_GPIO0, 10000000);
 	HAL_CM_Start(CM_GPIO0);
+	delay_ms(100);
 	
 	// Setup I2C with a clock of 100KHz to transmit 100Kbps
 	HAL_I2C_Set_Baud(I2C1, 100000);
@@ -41,15 +46,15 @@ int main()
 	while(1)
 	{
 		// Verify if OV7670 is connected to the i2c bus
-		if( !HA_I2C_Is_Connected(I2C1, OV7670_ADDRESS) )
+		if( !HAL_I2C_Is_Connected(I2C1, OV7670_ADDRESS) )
 		{
 			printf("Error(@I2C) - OV7670 was not found!\n");
 			break;
 		}
 		
 		// Ask user for register
-		uint8_t reg_addr = 0x00;
-		uint8_t data = 0x00;
+		uint32_t reg_addr = 0x00;
+		uint32_t data = 0x00;
 		printf("Register to write: ");
 		scanf("%d", &reg_addr);
 		printf("Value to write: ");
@@ -71,14 +76,14 @@ int main()
 			printf("Error(@OV7670): Invalid register address\n");
 		}
 		
-		
 		printf("Register to read: ");
 		scanf("%d", &reg_addr);
 		// Reg validation
 		if(reg_addr >= OV7670_LOWER_REG && reg_addr <= OV7670_UPPER_REG)
 		{
 			// Read register
-			err = HAL_I2C_ReadReg(I2C1, reg_addr, &data);
+			uint8_t rxdata;
+			eI2CError err = HAL_I2C_ReadReg(I2C1, reg_addr, &rxdata);
 			
 			// Verify reading errors
 			if(err != I2C_ERROR_NONE)
@@ -86,7 +91,7 @@ int main()
 				printf("Error(@I2C): Unable to read\n");
 			} else 
 			{
-				printf("Register 0x%02X contains 0x%02X\n", reg_addr, data);
+				printf("Register 0x%02X contains 0x%02X\n", reg_addr, rxdata);
 			}
 		} else
 		{
