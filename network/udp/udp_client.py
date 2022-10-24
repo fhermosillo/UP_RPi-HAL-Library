6687a50e724c3a0a71538042daec96876893f689
Fernando Hermosillo Reynoso <45222157+fhermosillo@users.noreply.github.com>
2022-10-24 18:49:20 -0500

    Add files via upload

---
+++ network/udp/udp_client.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 12 01:07:52 2022
+
+@author: User123
+"""
+
+from socket import *
+
+s = socket(AF_INET, SOCK_DGRAM)
+serverAddressPort   = ("169.254.154.192", 20001)
+
+
+s.sendto(str.encode('Hola raspy!'), serverAddressPort)
+udpmsg=s.recvfrom(1024)
+msg=udpmsg[0]
+addr=udpmsg[1]
+print('Server: ', addr, ', Message: ', msg)
+s.sendto(str.encode('Como estas?'), serverAddressPort)
+udpmsg=s.recvfrom(1024)
+msg=udpmsg[0]
+addr=udpmsg[1]
+print('Server: ', addr, ', Message: ', msg)
+s.close()
