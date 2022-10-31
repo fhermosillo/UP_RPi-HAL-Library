fd693e08fd20b324b5078d28dfe6fa7d1eb87a1f
Fernando Hermosillo Reynoso <45222157+fhermosillo@users.noreply.github.com>
2022-10-31 13:05:19 -0600

    Update main.c

--- network/C/3 - udp_client/main.c
+++ network/C/3 - udp_client/main.c
@@ -50,7 +50,7 @@ int main()
 		
 		char rxmsg[1024];
 		int rxlen = 0;
-		if((rxlen = SocketPi_ReceiveFrom(hClientSocket, rxmsg, 1024,NULL,NULL,PISOCKET_NO_TIMEOUT)) < 0)
+		if((rxlen = SocketPi_ReceiveFrom(hClientSocket, rxmsg, 1024,NULL,NULL,SOCKETPI_NO_TIMEOUT)) < 0)
 		{
 			puts("Reply failed");
 			break;
